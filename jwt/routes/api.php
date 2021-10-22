da155622b576481113f6b549e5db9d00f29c0e73
AR Shahin <mdshahinmije96@gmail.com>
2021-10-22 19:14:46 +0600

    =JWT auth

--- jwt/routes/api.php
+++ jwt/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 
@@ -11,10 +12,28 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::middleware(['auth:api'])->group(function () {
+    # Product
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{product}', [ProductController::class, 'show']);
 
-Route::get('/cart', [CartController::class, 'index']);
-Route::post('/cart', [CartController::class, 'store']);
-Route::delete('/cart/{productId}', [CartController::class, 'destroy']);
-Route::delete('/cart', [CartController::class, 'destroyAll']);
+    # Cart
+    Route::get('/cart', [CartController::class, 'index']);
+    Route::post('/cart', [CartController::class, 'store']);
+    Route::delete('/cart/{productId}', [CartController::class, 'destroy']);
+    Route::delete('/cart', [CartController::class, 'destroyAll']);
+
+    # Auth
+    Route::prefix('auth')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::get('/me', [AuthController::class, 'userProfile']);
+    });
+});
+
+
+# Auth
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
